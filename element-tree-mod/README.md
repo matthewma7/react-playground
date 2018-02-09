b41e7d9568d68354cb105e27fcb8d17200d33cc3
Matthew Ma <matthew.ma@kitware.com>
2018-02-09 12:39:24 -0500

    Add some improvements

--- element-tree-mod/README.md
+++ element-tree-mod/README.md
@@ -1,13 +1,30 @@
-# How does this work
+# What is this
+
+This is a demo showing a method to modify jsx output at runtime. 
+The core logic is inside the [wrap.js](https://github.com/matthewma7/react-playground/blob/master/element-tree-mod/src/wrap.js)
+
+A wrap function that patches the original render() method of a React Component.
+
+Before the patch, the output looks like this.
+
+![Image of element tree](https://github.com/matthewma7/react-playground/blob/master/element-tree-mod/before.png)
+
+After the patch, the output become this.
+
+![Image of element tree](https://github.com/matthewma7/react-playground/blob/master/element-tree-mod/after.png)
+
+# How does it work
 
 React jsx are just a tree of JavaScript object at runtime, so it is easy to manipulate them.
-The tree looks like this.
-![Image of element tree](
-https://github.com/matthewma7/react-playground/blob/master/element-tree-mod/element-tree.png)
 
-The code in wrap.js implements an insert method it currently append the new element at the end of its parent, but with similar mechanism, we could have insertAfter() and remove()
+The tree looks like this.
+
+![Image of element tree](https://github.com/matthewma7/react-playground/blob/master/element-tree-mod/element-tree.png)
+
+The code in wrap.js implements an insert method, and it currently append the new element at the end of its parent, but with similar mechanism, we could have insertAfter() and remove()
+We could potentially modify the [sizzlejs](https://sizzlejs.com/) to make it work with the element tree to make the usage even easier.
 
 # Setup
 
-This is created with react-create-app.
-So you could execute *npm install* and *npm start*
+This is created with *react-create-app*.
+So you could execute *npm install* and *npm start* to start the demo.
--- element-tree-mod/src/wrap.js
+++ element-tree-mod/src/wrap.js
@@ -3,22 +3,23 @@ import { wrap, isArray } from 'lodash';
 import ButtonsComponent from './ButtonsComponent';
 
 var insert = (elementTree, target, element) => {
+    // With this, React won't complain the element needs a key
+    // see https://github.com/facebook/react/blob/467b1034ce8af6807e11deb9dfeca4d4e922ed82/packages/react/src/ReactElementValidator.js#L136
+    element._store.validated = true;
     if (!elementTree.props || !elementTree.props.children) {
         return elementTree;
     }
     if (elementTree === target) {
-        element._store.validated = true;
         let children = isArray(elementTree.props.children) ? elementTree.props.children : [elementTree.props.children];
         let newElement = React.cloneElement(target, target.props, [...children, element]);
         newElement._store.validated = true;
         return newElement;
     } else {
-        var changed = false;
-        var children = [];
-        var result;
         if (isArray(elementTree.props.children)) {
+            let changed = false;
+            let children = [];
             for (var childElementTree of elementTree.props.children) {
-                result = insert(childElementTree, target, element);
+                let result = insert(childElementTree, target, element);
                 children.push(result);
                 if (result !== childElementTree) {
                     changed = true;
@@ -28,28 +29,25 @@ var insert = (elementTree, target, element) => {
                 let newElement = React.cloneElement(elementTree, elementTree.props, children);
                 newElement._store.validated = true;
                 return newElement;
-            } else {
-                return elementTree;
             }
         } else {
-            result = insert(elementTree.props.children, target, element);
+            let result = insert(elementTree.props.children, target, element);
             if (result !== elementTree.props.children) {
                 let newElement = React.cloneElement(elementTree, elementTree.props, result);
                 newElement._store.validated = true;
                 return newElement;
-            } else {
-                return elementTree;
             }
         }
+        return elementTree;
     }
 }
 
 ButtonsComponent.prototype.render = wrap(ButtonsComponent.prototype.render, function (render) {
-    var elementTree = render();
+    let elementTree = render();
     console.log(elementTree);
     // elementTree, is root from the render()
     // the elementTree.props.children, in this case, is the btn-group div
-    elementTree = insert(elementTree, elementTree.props.children, <button type='button' key='new' className='btn btn-primary'>New Button</button>);
+    elementTree = insert(elementTree, elementTree.props.children, <button type='button' className='btn btn-primary'>New Button</button>);
     elementTree = insert(elementTree, elementTree, <p>New Paragraph</p>);
     console.log(elementTree);
     return elementTree;
